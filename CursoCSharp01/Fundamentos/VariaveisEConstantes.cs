840c0880d6b380d1537a6969f076ee15a65a494d
Lucas <lvksounds@gmail.com>
2022-09-22 16:44:54 -0300

    primeiros conceitos do curso

---
+++ CursoCSharp01/Fundamentos/VariaveisEConstantes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp01.Fundamentos
+{
+    internal class VariaveisEConstantes
+    {
+        public static void Executar()
+        {
+            // area da circunferencia 
+            double raio = 4.5;
+            const double PI = 3.14;
+
+            double area = PI * raio * raio;
+            Console.WriteLine(area);
+            Console.WriteLine("A area é: " + area);
+
+
+            //tipos internos
+            // byte - 1 byte
+            // short - 2 bytes
+            // int - 4 bytes
+            // long - 8 bytes
+            bool estaChovendo = true;
+
+            Console.WriteLine("Está chovendo " + estaChovendo);
+
+            byte idade = 45;
+        }
+    }
+}
---
+++ CursoCSharp01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CursoCSharp01.Fundamentos;
+
+
+namespace CursoCSharp01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Variaveis e Constantes - Fundamentos", VariaveisEConstantes.Executar}
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
