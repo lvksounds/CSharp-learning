b4f51c37ce5268dab26cf63a07d09ec7bcd1146d
Lucas <lvksounds@gmail.com>
2022-11-29 16:58:14 -0300

    .

--- CursoCSharp01/OO/Encapsulamento.cs
+++ CursoCSharp01/OO/Encapsulamento.cs
@@ -5,9 +5,41 @@ namespace CursoCSharp01.OO
 {
     class Encapsulamento
     {
+        public class FilhoNaoReconhecido : SubCelebridade
+        {
+            public new void MeusAcessos()
+            {
+                Console.WriteLine("Filho nao Reconhecido");
+
+                Console.WriteLine(InfoPublica);
+                Console.WriteLine(JeitoDeFalar);
+                Console.WriteLine(CorDosOlhos);
+                //Console.WriteLine(NumerodoCelular);
+                //Console.WriteLine(SegredoFamilia);
+                //Console.WriteLine(UsoMuitoPhotoshop);
+            }
+            public class AmigoDistante
+            {
+                public readonly SubCelebridade amiga = new SubCelebridade();
+                public void MeusAcessos()
+                {
+                    Console.WriteLine(amiga.InfoPublica);
+                    //Console.WriteLine(amiga.CorDoOlho);
+                    //Console.WriteLine(amiga.NumerodoCelular);
+                    //Console.WriteLine(amiga.JeitoDeFalar);
+                    //Console.WriteLine(amiga.SegredoDefamilia);
+                    //Console.WriteLine(amiga.UsoMuitPhotoshop);
+
+                }
+            }
+        }
         public static void Executar()
         {
+            SubCelebridade sub = new SubCelebridade();
+            sub.MeusAcessos();
 
+            new FilhoReconhecido.MeusAcessos();
+            new AmigoProximo.MeusAcessos();
         }
     }
 }
